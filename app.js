0515ba5a9f96ba886951d897c9428d4520e06520
Ata-92 <atakal92@gmail.com>
2021-04-09 13:30:08 +0300

    Shorten numberCheck function

--- app.js
+++ app.js
@@ -18,58 +18,31 @@ button.addEventListener("click", numberCheck);
 function numberCheck() {
   if (input.value == "") {
     alert("Please enter a number : ");
-  } else {
-    if (attempt[1] > 1) {
-      if (input.value < 1 || input.value > 99 || isNaN(input.value) || parseInt(input.value) != input.value) {
-        confirm(`Your entry ${input.value} is invalid.\nPlease enter an integer number : `);
-        input.value = "";
-        attempt[0] += 1;
-        attempt[1] -= 1;
-        used.textContent = attempt[0];
-        remaining.textContent = attempt[1];
-      } else if (input.value < number) {
-        alert(`Your guess ${input.value} is low`);
-        input.value = "";
-        attempt[0] += 1;
-        attempt[1] -= 1;
-        used.textContent = attempt[0];
-        remaining.textContent = attempt[1];
-      } else if (input.value > number) {
-        alert(`Your guess ${input.value} is high`);
-        input.value = "";
-        attempt[0] += 1;
-        attempt[1] -= 1;
-        used.textContent = attempt[0];
-        remaining.textContent = attempt[1];
-      } else if (input.value == number) {
-        alert(`Your guess ${input.value} is right.
-              CONGRATS!!!`);
-        input.value = "";
-        number = Math.trunc(Math.ceil(Math.random() * 99));
-        attempt[0] = 0;
-        attempt[1] = 10;
-        used.textContent = attempt[0];
-        remaining.textContent = attempt[1];
-      }
-    } else {
-      if (input.value != number) {
-        alert(`You failed. The number was ${number}. Try again`)
-        input.value = "";
-        number = Math.trunc(Math.ceil(Math.random() * 99));
-        attempt[0] = 0;
-        attempt[1] = 10;
-        used.textContent = attempt[0];
-        remaining.textContent = attempt[1];
-      } else {
-        alert(`Your guess ${input.value} is right.
-              CONGRATS!!!`);
-        input.value = "";
-        number = Math.trunc(Math.ceil(Math.random() * 99));
-        attempt[0] = 0;
-        attempt[1] = 10;
-        used.textContent = attempt[0];
-        remaining.textContent = attempt[1];
-      }
+  } else if (attempt[1] > 1 && input.value != number) {
+    if (input.value < 1 || input.value > 99 || isNaN(input.value) || parseInt(input.value) != input.value) {
+      confirm(`Your entry ${input.value} is invalid.\nPlease enter an integer number : `);
+    } else if (input.value < number) {
+      alert(`Your guess ${input.value} is low`);
+    } else if (input.value > number) {
+      alert(`Your guess ${input.value} is high`);
     }
+    input.value = "";
+    attempt[0] += 1;
+    attempt[1] -= 1;
+    used.textContent = attempt[0];
+    remaining.textContent = attempt[1];
+  } else {
+    if (input.value == number) {
+      alert(`Your guess ${input.value} is right.
+              CONGRATS!!!`);
+    } else {
+      alert(`You failed. The number was ${number}. Try again`);
+    }
+    input.value = "";
+    number = Math.trunc(Math.ceil(Math.random() * 99));
+    attempt[0] = 0;
+    attempt[1] = 10;
+    used.textContent = attempt[0];
+    remaining.textContent = attempt[1];
   }
 }
